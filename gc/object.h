2ac65066ccf8b11eccfb01ed6808d808aef82469
Stephen Chou <s3chou@csail.mit.edu>
2020-10-13 23:50:13 -0400

    a4 updates

---
+++ gc/object.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "gc.h"
+
+#include <string>
+
+class Object : public Collectable {
+public:
+  Object() {
+    allocated++;
+  }
+
+  virtual ~Object() {
+    deallocated++;
+  }
+
+  static int64_t getAlive() {
+    return (allocated - deallocated);
+  }
+
+  Object* lhs = nullptr;
+  Object* rhs = nullptr;
+
+protected:
+  virtual void follow(CollectedHeap& heap) override {
+    if (lhs) heap.markSuccessors(lhs);
+    if (rhs) heap.markSuccessors(rhs);
+  };
+
+private: 
+  static size_t allocated;
+  static size_t deallocated;
+};
+
+class Integer : public Object {
+public:
+  Integer(int val) : val(val) {}
+
+  int val;
+};
+
+class String : public Object {
+public:
+  String(std::string val) : val(val) {}
+
+  std::string val;
+};
