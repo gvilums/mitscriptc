a66af99fbf4d49e30796eb6d6f62d30483605270
jakob-nogler <jakob.nogler@gmail.com>
2021-11-27 23:34:42 +0100

    fix const prop

--- p5/src/const_propagator.cpp
+++ p5/src/const_propagator.cpp
@@ -2,12 +2,14 @@
 #include "ir.h"
 #include "const_propagator.h"
 #include <set>
+#include "irprinter.h"
 
 ConstPropagator::ConstPropagator(IR::Program* prog) : prog_(prog){}
 
-bool ConstPropagator::propagate_instruction(IR::Instruction ins, std::unordered_map<size_t, runtime::Value> &const_var) {
+bool ConstPropagator::propagate_instruction(IR::Instruction &ins, std::unordered_map<size_t, runtime::Value> &const_var) {
     bool can_eliminate = true;
-    for (auto& arg : ins.args) {
+    IR::Instruction new_ins = ins;
+    for (auto& arg : new_ins.args) {
         if (arg.type == IR::Operand::VIRT_REG && const_var.count(arg.index))  {
             size_t val_idx = find(prog_->immediates.begin(), prog_->immediates.end(), const_var[arg.index]) - prog_->immediates.begin();
             arg = {IR::Operand::IMMEDIATE, int32_t(val_idx)};
@@ -17,6 +19,7 @@ bool ConstPropagator::propagate_instruction(IR::Instruction ins, std::unordered_
             can_eliminate = false;
         }
     }
+    ins = new_ins;
 
     if (!can_eliminate)
         return false;
@@ -66,46 +69,63 @@ bool ConstPropagator::propagate_instruction(IR::Instruction ins, std::unordered_
             return false;
     }
 
+    // std::cout << ins.out.index << " "  << runtime::value_get_std_string(new_value) << std::endl;
     const_var[ins.out.index] = new_value;
+    if (!count(prog_->immediates.begin(), prog_->immediates.begin(), new_value))
+        prog_->immediates.push_back(new_value);
     return true;
 }
 
 bool ConstPropagator::eliminate_assert(IR::Instruction ins, std::unordered_map<size_t, runtime::Value> &const_var) {
 
+    if (ins.op != IR::Operation::ASSERT_BOOL && ins.op != IR::Operation::ASSERT_INT && ins.op != IR::Operation::ASSERT_STRING && ins.op != IR::Operation::ASSERT_CLOSURE && ins.op != IR::Operation::ASSERT_NONZERO && ins.op != IR::Operation::ASSERT_RECORD)
+        return false;
+
     runtime::Value imm;
-    if (ins.args[0].type == IR::Operand::VIRT_REG && const_var.count(ins.args[0].index))
+    if (ins.args[0].type == IR::Operand::VIRT_REG && const_var.count(ins.args[0].index)) {
         imm = const_var[ins.args[0].index];
+    }
     else if (ins.args[0].type == IR::Operand::IMMEDIATE)
         imm = prog_->immediates[ins.args[0].index];
     else
         return false;
-
+      
     switch (ins.op) {
         case IR::Operation::ASSERT_BOOL:
             if (runtime::value_get_type(imm) != runtime::ValueType::Bool)
                 throw std::string("IllegalCastException");
+            return true;
             break;
         case IR::Operation::ASSERT_INT:
             if (runtime::value_get_type(imm) != runtime::ValueType::Int)
                 throw std::string("IllegalCastException");
+            return true;
             break;
         case IR::Operation::ASSERT_STRING:
             if (runtime::value_get_type(imm) != runtime::ValueType::HeapString && runtime::value_get_type(imm) != runtime::ValueType::InlineString)
                 throw std::string("IllegalCastException");  
+            return true;
             break;
         case IR::Operation::ASSERT_CLOSURE:
             if (runtime::value_get_type(imm) != runtime::ValueType::Closure)
                 throw std::string("IllegalCastException");
+            return true;
+            break;
+        case IR::Operation::ASSERT_RECORD:
+            if (runtime::value_get_type(imm) != runtime::ValueType::Record)
+                throw std::string("IllegalCastException");
+            return true;
             break;
         case IR::Operation::ASSERT_NONZERO:
             if (runtime::value_get_type(imm) != runtime::ValueType::Int || runtime::value_get_int32(imm) == 0)
                 throw std::string("DivideByZero");
+            return true;
             break;
         default:
             return false;
     }
 
-    return true;
+    return false;
 }
 
 IR::Program* ConstPropagator::optimize() {
@@ -116,26 +136,30 @@ IR::Program* ConstPropagator::optimize() {
 void ConstPropagator::propagate_const() {
     for (auto& fun : prog_->functions) {
         std::unordered_map<size_t, runtime::Value> const_var;
-        for (size_t idx = 0; idx < fun.blocks.size() - 1; idx++) {
+        for (size_t idx = 0; idx < fun.blocks.size(); idx++) {
             IR::BasicBlock new_block;
 
+            
             for (const auto& pn : fun.blocks[idx].phi_nodes) {
                 IR::PhiNode new_phi_node = pn;
-                for (auto& arg : new_phi_node.args)
+                for (auto& arg : new_phi_node.args) {
                     if (arg.second.type == IR::Operand::VIRT_REG && const_var.count(arg.second.index)) {
                         size_t val_idx = find(prog_->immediates.begin(), prog_->immediates.end(), const_var[arg.second.index]) - prog_->immediates.begin();
                         arg  = {arg.first, {IR::Operand::IMMEDIATE, int32_t(val_idx)}};
                     }
+                }
                 new_block.phi_nodes.push_back(new_phi_node);
             }
 
             for (int i = 0; i < fun.blocks[idx].instructions.size(); i++) {
                 IR::Instruction ins = fun.blocks[idx].instructions[i];
                 
-                if (propagate_instruction(ins, const_var))
+                if (propagate_instruction(ins, const_var)) {
                     continue;
-                if (eliminate_assert(ins, const_var))
+                }
+                if (eliminate_assert(ins, const_var)) {
                     continue;
+                }
 
                 new_block.instructions.push_back(ins);
             }
