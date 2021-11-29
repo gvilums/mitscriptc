a41cadd01b0dfd53f5a8920ead3e33afcd887200
Georgijs Vilums <georgijs.vilums@gmail.com>
2021-11-29 13:26:21 -0500

    update test script

--- p5/itest/bench.sh
+++ p5/itest/bench.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 INTERPRETER=$1 # don't forget a ./ preceeding your interp e.g. ./mitscript
+TIMEOUT=40
 
 for filename in bench/*.mit; do
     echo $filename
     if test -f $filename.input; then
-        time $INTERPRETER -s $filename < $filename.input > tmp1.out 2> tmp1.err
+        timeout $TIMEOUT $INTERPRETER --opt=all -s $filename < $filename.input > tmp1.out 2> tmp1.err
     else
-        time $INTERPRETER -s $filename > tmp1.out 2> tmp1.err
+        timeout $TIMEOUT $INTERPRETER --opt=all -s $filename > tmp1.out 2> tmp1.err
     fi
     CODE=$?
     if diff tmp1.out $filename.output; then
